5344c494f63734fff5e9de52de86d5c403a74588
sonnytricky <sonnytricky@gmail.com>
2026-04-20 16:03:53 +0200

    erweitert

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,24 @@
 export default [
   {
     files: ["**/*.js"],
+
     languageOptions: {
       ecmaVersion: "latest",
-      sourceType: "module"
+      sourceType: "module",
+      globals: {
+        document: "readonly",
+        window: "readonly"
+      }
     },
+
     rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error",
       indent: ["error", 2],
+      quotes: ["error", "double"],
       semi: ["error", "always"],
-      quotes: ["error", "double"]
+      "eol-last": ["error", "always"],
+      "no-console": "off"
     }
   }
 ];
